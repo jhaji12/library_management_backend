87cc9a02a25a8b92023961ad03306f8931accf1c
jhaji12 <jyoti.jhaji3@gmail.com>
2024-04-29 05:18:18 +0530

    debug true

--- library_management_backend/settings.py
+++ library_management_backend/settings.py
@@ -32,11 +32,11 @@ DATABASES = {
 SECRET_KEY = 'django-insecure-^exqbk*o+yn)p-9!$*fzlh4a$ldm$$j14hx($6dakj7+l981k^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', 'now.sh', '.floxus.co', "https://library-management-backend-i55e.onrender.com/"]
+# ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', 'now.sh', '.floxus.co', "https://library-management-backend-i55e.onrender.com/"]
 
-# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
