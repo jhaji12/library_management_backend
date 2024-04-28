98a0b266e6e40444a71818a5d85434390de394e7
jhaji12 <jyoti.jhaji3@gmail.com>
2024-04-28 23:34:42 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,5 @@
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
-# Apply database migrations (if necessary)
-# /path/to/python3.11 manage.py migrate
-
 # Collect static files
 python3.9 manage.py collectstatic --noinput
