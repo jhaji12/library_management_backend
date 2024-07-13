78fe28b2508e8f81a27946b855b46ddf6f4c2626
jhaji12 <jyoti.jhaji3@gmail.com>
2024-07-14 01:53:59 +0530

    dummy

--- library_management_backend/settings.py
+++ library_management_backend/settings.py
@@ -24,9 +24,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', 'now.sh', '.floxus.co', ".onrender.com", "4.213.40.183"]
 
-ALLOWED_HOSTS = ['library_management.toystack.dev', '127.0.0.1', 'localhost']
-
-CSRF_TRUSTED_ORIGINS = ['https://library_management.toystack.dev']
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
