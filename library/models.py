51d068993f0f0e257862ddb434ea6fc2337cf112
jhaji12 <jyoti.jhaji3@gmail.com>
2024-07-15 01:21:58 +0530

    added user edit in admin portal

--- library/models.py
+++ library/models.py
@@ -20,7 +20,7 @@ class User(AbstractUser):
     school = models.ForeignKey(School, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.get_level_display()
+        return self.username
 
 class Book(models.Model):
     book_id = models.CharField(max_length=20, unique=True)
